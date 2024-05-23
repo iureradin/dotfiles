ba648271511eb71fd29a5a01328b82c4b0c3c55f
Iure Radin <iureradin@gmail.com>
2024-05-22 23:15:54 -0300

    add fonte

--- configure_wsl2.sh
+++ configure_wsl2.sh
@@ -13,6 +13,8 @@ echo -e "\e[32m---> \e[0m Instalando git..."  && sudo apt install unzip -y
 echo -e "\e[32m---> \e[0m Instalando wget..."  && sudo apt install wget -y
 echo -e "\e[32m---> \e[0m Instalando net-tools..."  && sudo apt install net-tools -y
 echo -e "\e[32m---> \e[0m Instalando zsh..."  && sudo apt install zsh -y
+echo -e "\e[32m---> \e[0m Instalando curl..."  && sudo apt install curl -y
+echo -e "\e[32m---> \e[0m Instalando vim..."  && sudo apt install vim -y
 echo -e "\e[32m---> \e[0m Instalando awscli..."  && sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && sudo unzip awscliv2.zip && sudo ./aws/install
 
 echo -e "\e[32m---> \e[0m Instalando Oh My Zsh..." && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -22,16 +24,13 @@ echo -e "\e[32m---> \e[0m Instalando plugin zsh fzf..." && git clone --depth 1 h
 echo -e "\e[32m---> \e[0m Instalando tema do zsh powerlevel10k..." && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo -e "\e[32m---> \e[0m Instalando gerenciador de plugins do vim..." && curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo -e "\e[32m---> \e[0m Baixando dotfiles..."
-rm -rf /tmp/dotfiles
-git clone https://github.com/iureradin/dotfiles.git /tmp/dotfiles
 mkdir -p ~/.backup
 
 cp ~/.vimrc ~/.backup
-cp /tmp/dotfiles/.vimrc ~/.vimrc
+cp .vimrc ~/.vimrc
 
 cp ~/.zshrc ~/.backup
-cp /tmp/dotfiles/.zshrc ~/.zshrc
+cp .zshrc ~/.zshrc
 
 
 echo -e "\e[32mPor favor abra o vim e execute o comando :PlugInstall \e[0m"
