bdc361d8489ef49978ff6ba3e9385752ea8a008c
Iure Radin <iureradin@gmail.com>
2024-05-28 00:11:35 -0300

    ajust

--- configure_wsl2.sh
+++ configure_wsl2.sh
@@ -8,14 +8,7 @@ if [ "$(lsb_release -si)" != "Ubuntu" ]; then
 fi
 
 echo -e "\e[32m---> \e[0m Atualizando sistema..."  && sudo apt update -y
-echo -e "\e[32m---> \e[0m Instalando git..."  && sudo apt install git -y
-echo -e "\e[32m---> \e[0m Instalando git..."  && sudo apt install unzip -y
-echo -e "\e[32m---> \e[0m Instalando wget..."  && sudo apt install wget -y
-echo -e "\e[32m---> \e[0m Instalando net-tools..."  && sudo apt install net-tools -y
-echo -e "\e[32m---> \e[0m Instalando zsh..."  && sudo apt install zsh -y
-echo -e "\e[32m---> \e[0m Instalando curl..."  && sudo apt install curl -y
-echo -e "\e[32m---> \e[0m Instalando vim..."  && sudo apt install vim -y
-echo -e "\e[32m---> \e[0m Instalando yt-dlp..."  && sudo apt install yt-dlp -y
+echo -e "\e[32m---> \e[0m Instalando apps..."  && sudo apt install git unzip wget net-tools zsh curl vim yt-dlp -y
 echo -e "\e[32m---> \e[0m Instalando awscli..."  && sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && sudo unzip awscliv2.zip && sudo ./aws/install
 
 echo -e "\e[32m---> \e[0m Instalando Oh My Zsh..." && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
